cde5be5a1af1ea1f36926d900c39d2b21b99ce4c
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-06-09 23:41:50 +1000

    script

--- assets/script/script.js
+++ assets/script/script.js
@@ -25,8 +25,16 @@ function userInputs()
   {
     var password;
     console.log("userInputs()");
-    var length = window.prompt("Please choose a length of 8-128 charaters for a Password");
+    // var length = window.prompt("Please choose a length of 8-128 charaters for a Password");
+    
+    do{
+      var length = window.prompt("Please choose a length of 8-128 charaters for a Password");
+    }
+    while(length < 8 && length > 128 );
+
+    
     var lower = window.confirm("would you like to choose lowercase character");
+ 
     var upper = window.confirm("would you like to choose uppercase character");
     var numeric = window.confirm("would you like to choose numeric character");
     var special = window.confirm("would you like to choose special character");
@@ -52,46 +60,51 @@ function userInputs()
     console.log("upper " + upper);
     console.log("number " + numeric);
     console.log("special " + special);
-
+    var rules = [];
+     
     if(lower) {
       var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
-      console.log("lower alphabet   : ==> " + alphabet);
+      console.log("lower alphabet   : ==> " + lowerChar);
+      rules.push({chars: lowerChar, min : 3});
     }
     if(upper) {
       var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      console.log("upper alphabet   : ==> " + alphabet);
+      console.log("upper alphabet   : ==> " + upperChar);
+      rules.push({chars: upperChar, min : 2});
     }
     if(numeric) {
       var numericChar = '1234567890';
-      console.log("number alphabet   : ==> " + alphabet);
-    } //else if(input == 'alpha-numeric-caps') {
-    // alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    // } 
-    if(special) {
-      specialChar = '~!@#$%^&*()_+-=';
-
-      console.log("upper alphabet   : ==> " + alphabet);
+      console.log("number alphabet   : ==> " + numericChar);
+      rules.push({chars: numericChar, min : 2});
     } 
-      console.log("Chars alphabet   : ==> " + alphabet);
-      console.log("Chars alphabet_length   : ==> " + alphabet_length);
+    if(special) {
+      var specialChar = '~!@#$%^&*()_+-=';
 
+      console.log("upper alphabet   : ==> " + specialChar);
+      rules.push({chars: specialChar, min : 1});
+    } 
+    console.log("rules   : ==> " + rules);
+    for(let i = 0; i < rules.length; i++){
+      console.log(rules[i]);
       
+    }
+      return rules;
   }
 
 
 function generateSecurePassword(length, rules) {
-  if (!length || length == undefined) {
-      length = 8;
-  }
+  // if (!length || length == undefined) {
+  //     length = 8;
+  // }
 
-  if (!rules || rules == undefined) {
-      rules = [
-          {chars: "abcdefghijklmnopqrstuvwxyz", min: 3},  // As least 3 lowercase letters
-          {chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", min: 2},  // At least 2 uppercase letters
-          {chars: "0123456789", min: 2},                  // At least 2 digits
-          {chars: "!@#$&*?|%+-_./:;=()[]{}", min: 1}      // At least 1 special char
-      ];
-  }
+  // if (!rules || rules == undefined) {
+  //     rules = [
+  //         {chars: "abcdefghijklmnopqrstuvwxyz", min: 3},  // As least 3 lowercase letters
+  //         {chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", min: 2},  // At least 2 uppercase letters
+  //         {chars: "0123456789", min: 2},                  // At least 2 digits
+  //         {chars: "!@#$&*?|%+-_./:;=()[]{}", min: 1}      // At least 1 special char
+  //     ];
+  // }
 
   var allChars = "", allMin = 0;
   rules.forEach(function(rule) {

bfed0fc8893f29d7b7c848b32c9e8afaaab11d7a
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-06-10 15:28:38 +1000

    change js

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,7 +15,6 @@ function generatePassword()
 {
   // collecting the userinputs to generate password
   var userinputs = userInputs();
-  console.log("generatePassword   >>>> userinputs" + userinputs);
   // creating rules to generate the password
   var rules = generateRules(userinputs);
   // getting the length from the userinput.
@@ -33,11 +32,8 @@ function userInputs()
   {
     var password;
     var lengthInput=0;
-    console.log("userInputs()");
-    min=8;
-    max=128;
     do {
-        lengthInput = window.prompt("Please choose a lengthInput of 8-128 charaters for a Password");
+        lengthInput = window.prompt("Please choose a length between 8-128 charaters for a Password");
         console.log("lengthInput " + lengthInput);
     } while((lengthInput < 8) || (lengthInput >128));
 
@@ -48,13 +44,6 @@ function userInputs()
     var upper = window.confirm("Would you like to choose uppercase character");
     var numeric = window.confirm("Would you like to choose numeric character");
     var special = window.confirm("Would you like to choose special character");
- 
-    console.log("lengthInput " + lengthInput);
-    console.log("lower " + lower);
-    console.log("upper " + upper);
-    console.log("number " + numeric);
-    console.log("special " + special);
-
     return [lengthInput, lower,upper,numeric,special];
   }
 
@@ -69,34 +58,24 @@ function userInputs()
     var upper = userinputs[2];
     var numeric=userinputs[3];
     var special = userinputs[4];
-    console.log("lower " + lower);
-    console.log("upper " + upper);
-    console.log("number " + numeric);
-    console.log("special " + special);
     var rules = [];
      
     if(lower) {
       var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
-      console.log("lower alphabet   : ==> " + lowerChar);
       rules.push({chars: lowerChar, min : 3});
     }
     if(upper) {
       var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      console.log("upper alphabet   : ==> " + upperChar);
       rules.push({chars: upperChar, min : 2});
     }
     if(numeric) {
       var numericChar = '1234567890';
-      console.log("number alphabet   : ==> " + numericChar);
       rules.push({chars: numericChar, min : 2});
     } 
     if(special) {
       var specialChar = '~!@#$%^&*()_+-=';
-
-      console.log("upper alphabet   : ==> " + specialChar);
       rules.push({chars: specialChar, min : 1});
     } 
-    console.log("rules   : ==> " + rules);
     for(let i = 0; i < rules.length; i++){
       console.log(rules[i]);
       
@@ -115,9 +94,7 @@ function generateSecurePassword(length, rules) {
   var allChars = "", allMin = 0;
   rules.forEach(function(rule) {
       allChars += rule.chars;
-      console.log("generateSecurePassword  allChars : ==> " + allChars.length);
       allMin += rule.min;
-      console.log("generateSecurePassword  allMin : ==> " + allMin);
   });
  
   
@@ -125,18 +102,14 @@ function generateSecurePassword(length, rules) {
   var minLen = length - allMin;
   var j = 0;
   var k = 0;
-  console.log("heloooooooooooooooooooo");
   if(minLen <= allCharsLen)
   {
-    console.log("minLen <= allCharsLen   " + minLen <= allCharsLen);
-    console.log("allCharsLen   :- " + allCharsLen + ",minLen   :-    "+minLen );
         rules.push({chars: allChars, min: minLen});
   }
   else{
     console.log(" else            : ");
     var reminder = minLen % allCharsLen;
     var quotient = (minLen - reminder) / allCharsLen;
-    console.log("reminder   :- " + reminder + ",quotient   :-    "+quotient );
     for (; k < quotient; k++) {
       rules.push({chars: allChars, min: allCharsLen});
     }
@@ -147,21 +120,18 @@ function generateSecurePassword(length, rules) {
     
   }
 
-  
+  // verify the rules applied to the generator
   printRules(rules);
   var pswd = "";
   rules.forEach(function(rule) {
       if (rule.min > 0) {
-        console.log("generateSecurePassword  rule.min : ==> " + rule.min);
           pswd += shuffleString(rule.chars, rule.min);
-          console.log("generateSecurePassword  rule.chars : ==> " +rule.chars);
-          console.log("generateSecurePassword  pswd : ==> " + pswd);
       }
   });
   //return pswd after the shuffling operation
   return shuffleString(pswd, length);
 }
-
+// Print the charater type , rules as per input criteria -- Validation code.
 function printRules(rules){
 rules.forEach(function(rule) {
    console.log("  rule.chars : ==> " + rule.chars);

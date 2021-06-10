d4dfb09a65e679edfff237d46e2384decddfe29a
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-06-10 23:05:25 +1000

    code cleanup & Readme

--- README.md
+++ README.md
@@ -1,94 +1,50 @@
-# PasswordGenerator
+# Password Generator
 
-# 03 JavaScript: Password Generator
+This project generates the unique password based on the input criteria.
+This website takes the user input for the length and character types.
 
-## Your Task
+This website is developed using HTML, CSS and Javascript. 
 
-This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+Developing this project helps understading the java script window objects,evenlistener,javascript functions.
 
-The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+#installation
 
-## User Story
+Source code can be downloaded from https://github.com/SimonMSelvadurai/PasswordGenerator
 
-```
-AS AN employee with access to sensitive data
-I WANT to randomly generate a password that meets certain criteria
-SO THAT I can create a strong password that provides greater security
-```
+Application can be launched with the URL https://simonmselvadurai.github.io/PasswordGenerator/
 
-## Acceptance Criteria
+When the user clicks the Generate Password button from the UI, appropriate eventlistener has been
+clicked from the Javascript file. This is the files that holds the key business logic.
+Event click triggers back the UI to get the userinputs from the window promt.
 
-```
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-```
+User Input values has been validates and the corresponding rules of characterset has been framed.
+Total Length and the character set and the minimum characters per character set passed to the keylogic.
+This code is responsible to generate the random password. This password has been shuffled again and passed to the user.
 
-## Mock-Up
 
-The following image shows the web application's appearance and functionality:
 
-![The Password Generator application displays a red button to "Generate Password".](./Assets/03-javascript-homework-demo.png)
+#usage
 
-## Grading Requirements
+This project is to generates the unique password, based on the length and character set.
+User will be prompted to choose the inputs by button clicking the Generate Password button.
+User will be shown the window prompt to specify the total number of characters.
+User then propmted to decide atleast one charater set in the password.
+System will generate the password in combination of having atleast 1 character from the character set.
 
-This homework is graded based on the following criteria: 
+#credits
 
-### Technical Acceptance Criteria: 40%
+The following websites was used as reference to build this website.
 
-* Satisfies all of the preceding acceptance criteria plus the following:
+https://www.w3schools.com/css
 
-  * The homework should not produce any errors in the console when you inspect it using Chrome DevTools.
+https://www.w3schools.com/html
 
-### Deployment: 32%
+https://stackoverflow.com/questions
 
-* Application deployed at live URL.
+https://developer.mozilla.org/en-US/docs/Web/CSS
 
-* Application loads with no errors.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
 
-* Application GitHub URL submitted.
+#license
 
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository, with a unique name and a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+This is the open source project. User can make use of this to have required password generated.
--- assets/script/script.js
+++ assets/script/script.js
@@ -11,8 +11,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword()
-{
+function generatePassword() {
   // collecting the userinputs to generate password
   var userinputs = userInputs();
   // creating rules to generate the password
@@ -27,124 +26,149 @@ function generatePassword()
  * Get the user inputs like, length and character types
  * @ return user inputs
  */
-function userInputs()
-  {
-    var password;
-    var lengthInput=0;
-    do {
-        lengthInput = window.prompt("Please choose a length between 8-128 charaters for a Password");
-    } while((lengthInput < 8) || (lengthInput >128));
+function userInputs() {
+  var password;
+  var lengthInput = 0;
+  var isValid = false;
+  do {
+    lengthInput = parseInt(
+      window.prompt(
+        "Please choose a length between 8-128 charaters for a Password"
+      )
+    );
+    if (
+      lengthInput < 8 ||
+      lengthInput > 128 ||
+      isNaN(lengthInput) ||
+      lengthInput % 1 != 0
+    ) {
+      lengthInput = alert(
+        "Please choose a valid number between 8-128 charaters for a Password"
+      );
+    } else {
+      isValid = true;
+    }
+  } while (!isValid);
 
-
-    console.log((lengthInput < 8) && (lengthInput >128));
-
+  do {
     var lower = window.confirm("Would you like to choose lowercase character");
     var upper = window.confirm("Would you like to choose uppercase character");
     var numeric = window.confirm("Would you like to choose numeric character");
     var special = window.confirm("Would you like to choose special character");
-    return [lengthInput, lower,upper,numeric,special];
-  }
+    if (!(lower || upper || numeric || special)) {
+      lengthInput = alert("Please choose a anyone of the charater set ");
+      isValid = false;
+    } else {
+      isValid = true;
+    }
+  } while (!isValid);
 
+  return [lengthInput, lower, upper, numeric, special];
+}
+
+function validateForNumeric() {
+  var validator = /^[0-9]+$/g;
+  var nums = window.prompt("Please prompt any Numeric Number");
+  if (validator.test(nums)) {
+    specialChars();
+  }
+}
 /**
- * @param  array of userinputs 
+ * @param  array of userinputs
  * @returns rules
  */
 
-  function generateRules(userinputs)
-  {
-    var lower = userinputs[1];
-    var upper = userinputs[2];
-    var numeric=userinputs[3];
-    var special = userinputs[4];
-    var rules = [];
-     
-    if(lower) {
-      var lowerChar = 'abcdefghijklmnopqrstuvwxyz';
-      rules.push({chars: lowerChar, min : 3});
-    }
-    if(upper) {
-      var upperChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      rules.push({chars: upperChar, min : 2});
-    }
-    if(numeric) {
-      var numericChar = '1234567890';
-      rules.push({chars: numericChar, min : 2});
-    } 
-    if(special) {
-      var specialChar = '~!@#$%^&*()_+-=';
-      rules.push({chars: specialChar, min : 1});
-    } 
-    for(let i = 0; i < rules.length; i++){
-      console.log(rules[i]);
-      
-    }
-      return rules;
+function generateRules(userinputs) {
+  var lower = userinputs[1];
+  var upper = userinputs[2];
+  var numeric = userinputs[3];
+  var special = userinputs[4];
+  var rules = [];
+
+  if (lower) {
+    var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+    rules.push({ chars: lowerChar, min: 3 });
   }
+  if (upper) {
+    var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    rules.push({ chars: upperChar, min: 2 });
+  }
+  if (numeric) {
+    var numericChar = "1234567890";
+    rules.push({ chars: numericChar, min: 2 });
+  }
+  if (special) {
+    var specialChar = "~!@#$%^&*()_+-=";
+    rules.push({ chars: specialChar, min: 1 });
+  }
+  return rules;
+}
 
 /*
  * @param length , Rules array
- * Returns the generated password passed on the user input 
+ * Returns the generated password passed on the user input
  * Allocating minimum number of characters based on the criteria
  */
 
 function generateSecurePassword(length, rules) {
-  var allChars = "", allMin = 0;
-  rules.forEach(function(rule) {
-      allChars += rule.chars;
-      allMin += rule.min;
+  var allChars = "",
+    allMin = 0;
+  rules.forEach(function (rule) {
+    allChars += rule.chars;
+    allMin += rule.min;
   });
- 
-  
+
   var allCharsLen = allChars.length;
   var minLen = length - allMin;
   var j = 0;
   var k = 0;
-  if(minLen <= allCharsLen)
-  {
-        rules.push({chars: allChars, min: minLen});
-  }
-  else{
+  if (minLen <= allCharsLen) {
+    rules.push({ chars: allChars, min: minLen });
+  } else {
     var reminder = minLen % allCharsLen;
     var quotient = (minLen - reminder) / allCharsLen;
     for (; k < quotient; k++) {
-      rules.push({chars: allChars, min: allCharsLen});
+      rules.push({ chars: allChars, min: allCharsLen });
     }
-    
+
     if (reminder > 0) {
-      rules.push({chars: allChars, min: reminder});
+      rules.push({ chars: allChars, min: reminder });
     }
-    
   }
 
   // verify the rules applied to the generator
-  printRules(rules);
+  //printRules(rules);
   var pswd = "";
-  rules.forEach(function(rule) {
-      if (rule.min > 0) {
-          pswd += shuffleString(rule.chars, rule.min);
-      }
+  rules.forEach(function (rule) {
+    if (rule.min > 0) {
+      pswd += shuffleString(rule.chars, rule.min);
+    }
   });
   //return pswd after the shuffling operation
   return shuffleString(pswd, length);
 }
 // Print the charater type , rules as per input criteria -- Validation code.
-function printRules(rules){
-rules.forEach(function(rule) {
-   console.log("  rule.chars : ==> " + rule.chars);
-  console.log("  ruke.min : ==> " + rule.min);
-});
+function printRules(rules) {
+  rules.forEach(function (rule) {
+    console.log("  rule.chars : ==> " + rule.chars);
+    console.log("  ruke.min : ==> " + rule.min);
+  });
 }
 
 /*
-* @param rules strings, maximum length
-* @ returns the strings shuffled        
-*/
+ * @param rules strings, maximum length
+ * @ returns the strings shuffled
+ */
 
 function shuffleString(str, maxlength) {
-  var shuffledString = str.split('').sort(function(){
-    return 0.5-Math.random()}).join('');
+  var shuffledString = str
+    .split("")
+    .sort(function () {
+      return 0.5 - Math.random();
+    })
+    .join("");
   if (maxlength > 0) {
-      shuffledString = shuffledString.substr(0, maxlength);
+    shuffledString = shuffledString.substr(0, maxlength);
   }
   return shuffledString;
 }

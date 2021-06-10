08f993fd2b748c4b795ea08cded347f6569da9e2
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-06-10 11:00:29 +1000

    script

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
@@ -14,48 +13,62 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword()
 {
-  var userip = userInputs();
-  console.log("userip[0]" + userip[0]);
-  var rules = generateRules(userip);
-  var length = userip[0];
-   //var password = generateSecurePassword(length, rules);
-   var password = generateSecurePassword(length, rules);
+  // collecting the userinputs to generate password
+  var userinputs = userInputs();
+  console.log("generatePassword   >>>> userinputs" + userinputs);
+  // creating rules to generate the password
+  var rules = generateRules(userinputs);
+  // getting the length from the userinput.
+  var length = userinputs[0];
+  // generate the password passing the length and rules as parameter
+  var password = generateSecurePassword(length, rules);
+  console.log("generatePassword   >>>> password    : " + password);
+  return password;
 }
+/**
+ * 
+ * 
+ */
 function userInputs()
   {
     var password;
+    var lengthInput=0;
     console.log("userInputs()");
-    // var length = window.prompt("Please choose a length of 8-128 charaters for a Password");
-    
-    do{
-      var length = window.prompt("Please choose a length of 8-128 charaters for a Password");
-    }
-    while(length < 8 && length > 128 );
+    min=8;
+    max=128;
+    do {
+        lengthInput = window.prompt("Please choose a lengthInput of 8-128 charaters for a Password");
+        console.log("lengthInput " + lengthInput);
+    } while((lengthInput < 8) || (lengthInput >128));
 
-    
-    var lower = window.confirm("would you like to choose lowercase character");
+
+    console.log((lengthInput < 8) && (lengthInput >128));
+
+    var lower = window.confirm("Would you like to choose lowercase character");
+    var upper = window.confirm("Would you like to choose uppercase character");
+    var numeric = window.confirm("Would you like to choose numeric character");
+    var special = window.confirm("Would you like to choose special character");
  
-    var upper = window.confirm("would you like to choose uppercase character");
-    var numeric = window.confirm("would you like to choose numeric character");
-    var special = window.confirm("would you like to choose special character");
-    // window.alert("length : "+length "|"+ "lower"+lower +"upper"+upper +"number"+ number );\
-    console.log("length " + length);
+    console.log("lengthInput " + lengthInput);
     console.log("lower " + lower);
     console.log("upper " + upper);
     console.log("number " + numeric);
     console.log("special " + special);
 
-    var userInputs = [length, lower,upper,numeric,special];
-    return userInputs;
-
+    return [lengthInput, lower,upper,numeric,special];
   }
 
-  function generateRules(userInputs)
+/**
+ * @param  array of userinputs 
+ * @returns rules
+ */
+
+  function generateRules(userinputs)
   {
-    var lower = userInputs[1];
-    var upper = userInputs[2];
-    var numeric=userInputs[3];
-    var special = userInputs[4];
+    var lower = userinputs[1];
+    var upper = userinputs[2];
+    var numeric=userinputs[3];
+    var special = userinputs[4];
     console.log("lower " + lower);
     console.log("upper " + upper);
     console.log("number " + numeric);
@@ -91,45 +104,97 @@ function userInputs()
       return rules;
   }
 
+/*
+ * @param length , Rules array
+ * Returns the generated password passed on the user input 
+ * Allocating minimum number of characters based on the criteria
+ */
 
 function generateSecurePassword(length, rules) {
-  // if (!length || length == undefined) {
-  //     length = 8;
-  // }
-
-  // if (!rules || rules == undefined) {
-  //     rules = [
-  //         {chars: "abcdefghijklmnopqrstuvwxyz", min: 3},  // As least 3 lowercase letters
-  //         {chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", min: 2},  // At least 2 uppercase letters
-  //         {chars: "0123456789", min: 2},                  // At least 2 digits
-  //         {chars: "!@#$&*?|%+-_./:;=()[]{}", min: 1}      // At least 1 special char
-  //     ];
-  // }
-
+  console.log(" generateSecurePassword >>>> length   : ==> " + length);
   var allChars = "", allMin = 0;
   rules.forEach(function(rule) {
       allChars += rule.chars;
+      console.log("generateSecurePassword  allChars : ==> " + allChars.length);
       allMin += rule.min;
+      console.log("generateSecurePassword  allMin : ==> " + allMin);
   });
-  if (length < allMin) {
-      length = allMin;
-  }
-  rules.push({chars: allChars, min: length - allMin});
   
+  // if (length < allMin) {
+  //     length = allMin;
+  // }
+  
+  var allCharsLen = allChars.length;
+  var minLen = length - allMin;
+  var j = 0;
+  var k = 0;
+  console.log("heloooooooooooooooooooo");
+  if(minLen <= allCharsLen)
+  {
+    console.log("minLen <= allCharsLen   " + minLen <= allCharsLen);
+    console.log("allCharsLen   :- " + allCharsLen + ",minLen   :-    "+minLen );
+        rules.push({chars: allChars, min: minLen});
+  }
+  else{
+    console.log(" else            : ");
+    var reminder = minLen % allCharsLen;
+    var quotient = (minLen - reminder) / allCharsLen;
+    console.log("reminder   :- " + reminder + ",quotient   :-    "+quotient );
+    for (; k < quotient; k++) {
+      rules.push({chars: allChars, min: allCharsLen});
+    }
+    
+    if (reminder > 0) {
+      rules.push({chars: allChars, min: reminder});
+    }
+    // for( j=0; j<allCharsLen; j++)
+    // {
+
+    //   // k = k - allCharsLen;
+    //   if(k == allCharsLen){
+    //     rules.push({chars: allChars, min: allCharsLen});
+    //     console.log("kooooooooooooo" + k );
+    //     allCharsLen= minLen - allCharsLen;
+    //     console.log("allCharsLen   :- " + allCharsLen + ",minLen   :-    "+minLen );
+    //   }
+    // }
+  }
+
+  
+  printRules(rules);
   var pswd = "";
   rules.forEach(function(rule) {
       if (rule.min > 0) {
+        console.log("generateSecurePassword  rule.min : ==> " + rule.min);
           pswd += shuffleString(rule.chars, rule.min);
+          console.log("generateSecurePassword  rule.chars : ==> " +rule.chars);
+          console.log("generateSecurePassword  pswd : ==> " + pswd);
       }
   });
-  
-  return shuffleString(pswd);
+  //return pswd;
+  return shuffleString(pswd, length);
 }
 
+function printRules(rules){
+rules.forEach(function(rule) {
+   console.log("  rule.chars : ==> " + rule.chars);
+  console.log("  ruke.min : ==> " + rule.min);
+});
+}
+
+/*
+*
+*
+*/
+
 function shuffleString(str, maxlength) {
-  var shuffledString = str.split('').sort(function(){return 0.5-Math.random()}).join('');
+  console.log("shuffleString  maxlength : ==> " + maxlength);
+  console.log("shuffleString  str : ==> " + str);
+  var shuffledString = str.split('').sort(function(){
+    return 0.5-Math.random()}).join('');
   if (maxlength > 0) {
       shuffledString = shuffledString.substr(0, maxlength);
   }
+  console.log("shuffleString  shuffledString shuffledString shuffledString : ==> " + shuffledString);
   return shuffledString;
 }
